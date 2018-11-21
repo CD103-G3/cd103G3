68812cad9186f3eca2c677570468e24aebf622f1
BenHsiehHikari <32918807+BenHsiehHikari@users.noreply.github.com>
2018-11-21 20:32:59 +0800

    8:32 11/21

--- heartDataUpdate.php
+++ heartDataUpdate.php
@@ -5,15 +5,15 @@ try{
   require_once("connectMember.php");
 
 
-  if($_REQUEST["mealState"]=="false"){ //會員是否收藏過此餐點
+  if($_POST["mealState"]=="false"){ //會員是否收藏過此餐點
 
       //新增
-      $sql = "INSERT INTO membercoll (memberColl_No, member_No, meal_No) 
-              VALUES(NULL, :member_No, :meal_No)";
+      $sql = "INSERT INTO membercoll (member_No, meal_No) 
+              VALUES(:member_No, :meal_No)";
 
       $member = $pdo->prepare( $sql);
-      $member->bindValue("member_No", $_SESSION["member_No"]);
-      $member->bindValue("meal_No", substr($_POST["mealNo"], 2));
+      $member->bindValue(":member_No", $_SESSION["member_No"]);
+      $member->bindValue(":meal_No", substr($_POST["mealNo"], 2));
       $member->execute();
 
   }else{  
@@ -23,8 +23,8 @@ try{
             WHERE member_No = :member_No && meal_No = :meal_No";
 
     $member = $pdo->prepare( $sql);
-    $member->bindValue("member_No", $_SESSION["member_No"]);
-    $member->bindValue("meal_No", substr($_POST["mealNo"], 2));
+    $member->bindValue(":member_No", $_SESSION["member_No"]);
+    $member->bindValue(":meal_No", substr($_POST["mealNo"], 2));
     $member->execute();
 
   }
