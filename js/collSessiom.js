f498597f43bade29c0de22787c8bec7f8a88f744
jay00000zero <44055111+jay00000zero@users.noreply.github.com>
2018-11-21 20:50:54 +0800

    Add files via upload

--- js/collSessiom.js
+++ js/collSessiom.js
@@ -1,15 +1,18 @@
 var storage = sessionStorage;
+
 function doFirst(){
+
 	if(storage['addItemList']==null){
 		storage['addItemList'] ='';  //storage.setItem('addItemList','');
 	}
-	
 	// 幫給個add Cart建事件聆聽功能。
 	var list = document.querySelectorAll('.coll_food_shop'); //list是陣列
+	console.log(list);
 	for(var i=0; i<list.length;i++){
 		list[i].addEventListener('click',function(){
-			var Menus = document.querySelector('#'+this.id+' input').value; //('#A1001_input').value
-			addItem(this.id, Menus);
+			alert('該餐點已加入購物車');
+			var collsadd = document.querySelector('#'+this.id+' input').value; //('#A1001_input').value
+			addItem(this.id, collsadd);
 		});
 	}
 
